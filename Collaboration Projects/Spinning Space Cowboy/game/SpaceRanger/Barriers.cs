f00e416135c67e69cf318a29b1e5ff04f89b3355
Adam Gayheart (RIT Student) <ajg8716@rit.edu>
2024-01-05 15:47:36 -0500

    fixed group projects files

---
+++ Collaboration Projects/Spinning Space Cowboy/game/SpaceRanger/Barriers.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceRanger
+{
+    internal class Barriers
+    {
+        //-----------------------------------------------------------------------------------------------------------------------------------------
+        //THIS CLASS IS NOT IMPLEMENTED IN THE FINAL BUILD FOR THE TURN IN. WE PLANNED TO IMPLEMENT THIS FEATURE BUT DID NOT HAVE THE TIME TO DO SO
+        //-----------------------------------------------------------------------------------------------------------------------------------------
+
+        //FIELDS
+        //screen barrier
+        private GraphicsDeviceManager _graphics;
+
+
+        //creation
+        protected Texture2D barrierTexture;
+        protected Vector2 barrierPos;
+        protected Vector2 barrierOrigin;
+        protected Rectangle? barrierRect;
+        protected int barrierWidth;
+        protected int barrierHeight;
+
+        //Specific barrier dimensions based on types
+
+        //small rock 1
+        protected int barrier1Width = 19 * 6;
+        protected int barrier1Height = 14 * 6;
+        protected Vector2 barrier1Vect = new Vector2(0, 0);
+
+        //small rock 2
+        protected int barrier2Width = 16 * 6;
+        protected int barrier2Height = 14 * 6;
+        protected Vector2 barrier2Vect = new Vector2(0, 15 * 6);
+
+        //small rock 3
+        protected int barrier3Width = 19 * 6;
+        protected int barrier3Height = 14 * 6;
+        protected Vector2 barrier3Vect = new Vector2(0, 2*(15 * 6));
+
+        //huge rock
+        protected int barrier4Width = 43 * 6;
+        protected int barrier4Height = 55 * 6;
+        protected Vector2 barrier4Vect = new Vector2(20 * 6, 0);
+
+        //long rock
+        protected int barrier5Width = 22 * 6;
+        protected int barrier5Height = 49 * 6;
+        protected Vector2 barrier5Vect = new Vector2(64 * 6, 0);
+
+        //satellite
+        protected int barrier6Width = 60 * 6;
+        protected int barrier6Height = 33 * 6;
+        protected Vector2 barrier6Vect = new Vector2(0, 0);         
+        //Movement
+        protected float barrierSpeedX;
+        protected float barrierSpeedY;
+        protected float rotateSpeed;
+
+
+        //PROPERTIES
+        public Texture2D BarrierTexture
+        {
+            get { return barrierTexture; }
+        }
+
+        //CONSTRUCTOR
+        public Barriers(Texture2D texture, string barriertype, int x, int y, int width, int height, float rotate)
+        {
+            //texture for the barrier
+            this.barrierTexture = texture;
+
+            //vector for the barrier
+            this.barrierPos = new Vector2(x, y);
+
+            //width and height
+            this.barrierWidth = width;
+            this.barrierHeight = height;
+
+            //rectangle
+            barrierRect = new Rectangle(x, y, width, height);
+
+            //origin
+            this.barrierOrigin = new Vector2(width / 2, height / 2);
+
+            rotateSpeed = rotate;
+            barrierSpeedX = 1.0f;
+            barrierSpeedY = 1.0f;
+
+        }
+
+        //METHODS
+
+        /// <summary>
+        /// logic for what happens when a rock runs into something. should force the entities to bounch off the barrier and the bullets to count as hitting a target
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="e"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public bool BarrierCollide(Rectangle r)
+        {
+            //collide check logic that checks if the given rectangle intersects with the barriers rectangle 
+            return (r.Intersects(new Rectangle((int)this.barrierPos.X, (int)this.barrierPos.Y, this.barrierWidth, this.barrierHeight)));
+        }
+
+        public List<Rectangle> GetBarrierList(List<string> types, List<Rectangle> rects)
+        {
+            //initialize the list of rectangles as a new list
+            rects = new List<Rectangle>();
+
+            //check each string inside the list of the types of barriers
+            foreach(string type in types)
+            {
+                //determine which kind of barrier will be created from the list of types given
+                switch (type)
+                { 
+                    //small rock 1
+                    case "1":
+                        rects.Add(new Rectangle(0, 0, barrier1Width, barrier1Height));
+                        break;
+                    //small rock 2
+                    case "2":
+                        rects.Add(new Rectangle());
+                        break;
+                    //small rock 3
+                    case "3":
+
+                        break;
+                    //huge rock
+                    case "4":
+
+                        break;
+                    //long rock
+                    case "5":
+
+                        break;
+                    //satellite
+                    case "6":
+
+                        break;
+                }
+            }
+
+            return rects;
+        }
+
+        /// <summary>
+        /// default movement of the barriers
+        /// </summary>
+        public void Update()
+        {
+            //ambiently move barrier across screen
+            barrierPos.X += barrierSpeedX;
+            barrierPos.Y += barrierSpeedY;
+
+            //logic for barrier movement
+            //if the barrier goes over the right wall
+            if (barrierPos.X > _graphics.PreferredBackBufferWidth)
+            {
+                //sets the position on the left wall
+                barrierPos.X = 1;
+            }
+            //if the barrier goes over the left wall
+            if (barrierPos.X < 0)
+            {
+                //sets position on the right wall
+                barrierPos.X = _graphics.PreferredBackBufferWidth - barrierWidth;
+            }
+            //if the barrier goes over bottom wall
+            if (barrierPos.Y > _graphics.PreferredBackBufferHeight)
+            {
+                //sets position on top wall
+                barrierPos.Y = 1;
+            }
+            //if the barrier for over top wall
+            if (barrierPos.Y < 0)
+            {
+                //sets position to bottom wall
+                barrierPos.Y = _graphics.PreferredBackBufferHeight - barrierHeight;
+            }
+
+        }
+        /// <summary>
+        /// default draw for the barriers
+        /// </summary>
+        public void Draw(Barriers b, SpriteBatch sb)
+        {
+            //drawws barrier texture vector position  rectangle    color        rotate speed    rotate origin    scalar vector      no sprite effect   layer depth
+            sb.Draw(b.barrierTexture, b.barrierPos, b.barrierRect, Color.White, b.rotateSpeed, b.barrierOrigin, new Vector2 (1,1), SpriteEffects.None, 0f);
+        }
+    }
+}
---
+++ Collaboration Projects/Spinning Space Cowboy/game/SpaceRanger/Bullet.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+
+namespace SpaceRanger
+{
+    internal class Bullet
+    {
+        //bullet position, rotation, and movement fields
+        private Vector2 bulletVect;
+        private float bulletRotation;
+        private Vector2 direction;
+        private Vector2 newBulletVect;
+        private Rectangle bulletRect;
+
+        private int positionCheck = 0;
+        private int rotationCheck = 0;
+        private static int reloadCheck = 0;
+
+        private int aiPositionCheck = 0;
+        private int aiRotationCheck = 0;
+
+        //player speed
+        private const float playerSpeed = 10;
+
+        //arm's vector used for determining where bullets spawn
+        private Vector2 armVect;
+
+        //sprite things
+        private SpriteBatch sb;
+        private Texture2D spriteSheet;
+
+        //game state
+        public enum GameMode
+        {
+            Menu, Tutorial, Game, GameOver
+        }
+        public GameMode gameState;
+
+        //bullet's speed
+        private const float bulletspeed = 25;
+
+        //bullet's state
+        private bool bulletAlive;
+
+        //size of bullet
+        private int bulletWidth = 13 * 5;
+        private int bulletHeight = 3 * 5;
+
+        //mouse state to check if player shoots
+        private MouseState mouseState;
+
+        //Bullet properties
+        public Rectangle BulletRect
+        {
+            get { return bulletRect; }
+        }
+
+        //bool used to determine if bullet should be spawned
+        public bool BulletAlive
+        {
+            get { return BulletAlive; }
+            set { BulletAlive = value; }
+        }
+
+        //bool for checking if player needs to reload
+        public static int ReloadCheck
+        {
+            get { return reloadCheck; }
+            set { reloadCheck = value; }
+        }
+
+        //bullet constructor
+        public Bullet(Texture2D bulletSprite, Vector2 armVect)
+        {
+            this.spriteSheet = bulletSprite;
+            this.armVect = armVect;
+            this.bulletVect = new Vector2(armVect.X + 180, armVect.Y - 15);
+            this.bulletRect = new Rectangle((int)bulletVect.X, (int)bulletVect.Y, bulletWidth, bulletHeight);
+            bulletAlive = true;
+            direction = new Vector2();
+        }
+
+        /// <summary>
+        /// general shoot method for both player and AI, taken in who shot the bullet and where the player and enemy's vectors are 
+        /// </summary>
+        /// <param name="whoShoot"></param>
+        /// <param name="playerLoc"></param>
+        /// <param name="enemyVect"></param>
+        /// <param name="enemyArmVect"></param>
+        public void Shoot(bool whoShoot, Vector2 playerLoc, Vector2 enemyVect, Vector2 enemyArmVect)
+        {
+            //player code
+            if (whoShoot == true)
+            {
+                mouseState = Mouse.GetState();
+
+                //Math to figure out rotation. Finding the angle from player to mouse is done by finding the hypotenuse, (which is distance from player vector to mouse vector), and the opposite. Divide the opp by hyp, and use arcsine to find the angle from this triangle.
+                Vector2 mousePos = new Vector2(mouseState.X, mouseState.Y);
+                Vector2 origin = new Vector2(armVect.X + 44 / 2f, armVect.Y + 6 / 2f);
+
+                //Create the vector of the opposite so we can calculate the angle
+                Vector2 opposite = new Vector2(mousePos.X, origin.Y);
+
+                if (rotationCheck == 0)
+                {
+                    //This if else chain is to determine the correct calculation to use for each of the 4 quadrants of where the mouse can be in relation to the player: bottom right, bottom left, top right, and top left in that order.
+                    //determines what angle a bullet should be shot at
+                    if (mousePos.X > origin.X && mousePos.Y > origin.Y)
+                    {
+                        bulletRotation = (float)Math.Asin(Vector2.Distance(mousePos, opposite) / Vector2.Distance(mousePos, newBulletVect));
+                    }
+                    else if (mousePos.X > origin.X && mousePos.Y < origin.Y)
+                    {
+                        bulletRotation = (float)(-Math.Asin(Vector2.Distance(mousePos, opposite) / Vector2.Distance(mousePos, newBulletVect)));
+                            
+                        if (float.IsNaN(bulletRotation))
+                        {
+                            bulletRotation = -1.4f;
+                        }
+
+                    }
+
+                    
+                    if (mousePos.X < origin.X && mousePos.Y > origin.Y)
+                    {
+                        bulletRotation = -((float)-((Math.PI / 2.0) - Math.Asin(Vector2.Distance(mousePos, opposite) / Vector2.Distance(mousePos, newBulletVect)) + (Math.PI / 2.0)));
+
+                    }
+                    else if (mousePos.X < origin.X && mousePos.Y < origin.Y)
+                    {
+                        bulletRotation = -((float)((Math.PI / 2.0) - Math.Asin(Vector2.Distance(mousePos, opposite) / Vector2.Distance(mousePos, newBulletVect)) + (Math.PI / 2.0)));
+                    }
+                }
+            }
+            //enemy code
+            else
+            {
+                Vector2 origin = enemyArmVect;
+                bulletVect = new Vector2(enemyArmVect.X + 180, enemyArmVect.Y - 15);
+                
+                //Create the vector of the opposite so we can calculate the angle
+                Vector2 opposite = new Vector2(playerLoc.X, origin.Y);
+
+                if (aiRotationCheck == 0)
+                {
+                    //This if else chain is to determine the correct calculation to use for each of the 4 quadrants of where the mouse can be in relation to the player: bottom right, bottom left, top right, and top left in that order.
+                    //determines what angle the bullet should be shot from
+                    if (playerLoc.X > origin.X && playerLoc.Y > origin.Y)
+                    {
+                        bulletRotation = (float)Math.Asin(Vector2.Distance(playerLoc, opposite) / Vector2.Distance(playerLoc, enemyVect));
+                    }
+                    else if (playerLoc.X < origin.X && playerLoc.Y > origin.Y)
+                    {
+                        bulletRotation = (float)((Math.PI / 2.0) - Math.Asin(Vector2.Distance(playerLoc, opposite) / Vector2.Distance(playerLoc, enemyVect)) + (Math.PI / 2.0));
+                    }
+                    else if (playerLoc.X > origin.X && playerLoc.Y < origin.Y)
+                    {
+                        bulletRotation = (float)-Math.Asin(Vector2.Distance(playerLoc, opposite) / Vector2.Distance(playerLoc, enemyVect));
+                    }
+                    else if (playerLoc.X < origin.X && playerLoc.Y < origin.Y)
+                    {
+                        bulletRotation = (float)-((Math.PI / 2.0) - Math.Asin(Vector2.Distance(playerLoc, opposite) / Vector2.Distance(playerLoc, enemyVect)) + (Math.PI / 2.0));
+                    }
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// move the bullets on screen using the bullets speed and who shot the bullet
+        /// </summary>
+        /// <param name="whatBullet"></param>
+        /// <returns></returns>
+        public float Move(bool whatBullet)
+        {
+            //player code
+            if (whatBullet == true)
+            {
+                direction.X = (float)-(Math.Cos(bulletRotation) * playerSpeed);
+                direction.Y = (float)-(Math.Sin(bulletRotation) * playerSpeed);
+                
+                newBulletVect.X += direction.X;
+                newBulletVect.Y += direction.Y;
+
+                return bulletRotation;
+
+            }
+            //enemy code
+            else
+            {
+                direction.X = (float)(Math.Cos(bulletRotation));
+                direction.Y = (float)(Math.Sin(bulletRotation));
+                
+                newBulletVect.X += direction.X;
+                newBulletVect.Y += direction.Y;
+
+                return bulletRotation;
+            }
+        }
+
+        /// <summary>
+        /// checks for collision and calls the shoot method based on who is shooting
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="current"></param>
+        /// <param name="last"></param>
+        /// <param name="targets"></param>
+        /// <param name="enemies"></param>
+        /// <param name="aiShoot"></param>
+        /// <param name="playerLoc"></param>
+        /// <param name="enemyVect"></param>
+        /// <param name="enemyArmVect"></param>
+        public void Update(GameTime gameTime, MouseState current, MouseState last, List<Rectangle> targets, List<Rectangle> enemies, bool aiShoot, Vector2 playerLoc, Vector2 enemyVect, Vector2 enemyArmVect)
+        {
+            //collision code
+            if (targets != null)
+            {
+                foreach (Rectangle t in targets)
+                {
+                    if (bulletRect.Intersects(t))
+                    {
+                        bulletAlive = false;
+                    }
+                }
+            }
+            if (enemies != null)
+            {
+                foreach (Rectangle e in enemies)
+                {
+                    if (bulletRect.Intersects(e))
+                    {
+                        bulletAlive = false;
+                    }
+                }
+            }
+
+
+            //player shoot
+            if (aiShoot == false)
+            {
+                if (current.LeftButton == ButtonState.Pressed && last.LeftButton == ButtonState.Released)
+                {
+                    if (positionCheck == 0)
+                    {
+                        newBulletVect = bulletVect;
+                    }
+
+                    Shoot(true, playerLoc, new Vector2(10000, 10000), new Vector2(10000, 10000));
+                    
+                    positionCheck++;
+                    rotationCheck++;
+                }
+            }
+            //ai shoot
+            else if (aiShoot == true)
+            {
+                if (aiPositionCheck == 0)
+                {
+                    newBulletVect = bulletVect;
+                }
+               
+                Shoot(false, playerLoc, enemyVect, enemyArmVect);
+                
+                aiPositionCheck++;
+                aiRotationCheck++;
+            }
+        }
+
+        /// <summary>
+        /// draws the bullet based on the rotation 
+        /// </summary>
+        /// <param name="sb"></param>
+        public void Draw(SpriteBatch sb,Color color)
+        {
+            if (bulletAlive == true)
+            {
+                direction.X = (float)(Math.Cos(bulletRotation) * bulletspeed);
+                direction.Y = (float)(Math.Sin(bulletRotation) * bulletspeed);
+
+                newBulletVect += direction;
+                bulletRect = new Rectangle((int)newBulletVect.X, (int)newBulletVect.Y, bulletWidth, bulletHeight);
+
+                sb.Draw(spriteSheet,
+                    bulletRect,
+                                    new Rectangle(51 * 5, 16 * 5, bulletWidth, bulletHeight),
+                                    color,
+                                    bulletRotation,
+                                    new Vector2(bulletWidth / 2f, bulletHeight / 2f),
+                                    SpriteEffects.None,
+                                    0f);
+            }
+            else
+            {
+                bulletRect = new Rectangle(10000, 10000, bulletWidth, bulletHeight);
+            }
+        }
+    }
+}
